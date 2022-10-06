2deb487fee39e0f9499f02cdf5a818da73b58b67
ggtmags <103865103+ggtmags@users.noreply.github.com>
2022-10-06 13:43:05 -0400

    added smm2 controler

---
+++ dashboard/js/dashboards/smm2.js
@@ -0,0 +1,230 @@
+const skip_1_total_rep = nodecg.Replicant('skip_1_total');
+const clear_1_total_rep = nodecg.Replicant('clear_1_total');
+const seed_1_name_rep = nodecg.Replicant('seed_1_name');
+
+const skip_2_total_rep = nodecg.Replicant('skip_2_total');
+const clear_2_total_rep = nodecg.Replicant('clear_2_total');
+const seed_2_name_rep = nodecg.Replicant('seed_2_name');
+
+const skip_3_total_rep = nodecg.Replicant('skip_3_total');
+const clear_3_total_rep = nodecg.Replicant('clear_3_total');
+const seed_3_name_rep = nodecg.Replicant('seed_3_name');
+
+const skip_4_total_rep = nodecg.Replicant('skip_4_total');
+const clear_4_total_rep = nodecg.Replicant('clear_4_total');
+const seed_4_name_rep = nodecg.Replicant('seed_4_name');
+
+const skip_5_total_rep = nodecg.Replicant('skip_5_total');
+const clear_5_total_rep = nodecg.Replicant('clear_5_total');
+const seed_5_name_rep = nodecg.Replicant('seed_5_name');
+
+const skip_6_total_rep = nodecg.Replicant('skip_6_total');
+const clear_6_total_rep = nodecg.Replicant('clear_6_total');
+const seed_6_name_rep = nodecg.Replicant('seed_6_name');
+
+const skip_7_total_rep = nodecg.Replicant('skip_7_total');
+const clear_7_total_rep = nodecg.Replicant('clear_7_total');
+const seed_7_name_rep = nodecg.Replicant('seed_7_name');
+
+
+const skip_1_total = document.querySelector('#skip_1_total');
+const clear_1_total = document.querySelector('#clear_1_total');
+const seed_1_name = document.querySelector('#seed_1_name');
+const skip_2_total = document.querySelector('#skip_2_total');
+const clear_2_total = document.querySelector('#clear_2_total');
+const seed_2_name = document.querySelector('#seed_2_name');
+const skip_3_total = document.querySelector('#skip_3_total');
+const clear_3_total = document.querySelector('#clear_3_total');
+const seed_3_name = document.querySelector('#seed_3_name');
+const skip_4_total = document.querySelector('#skip_4_total');
+const clear_4_total = document.querySelector('#clear_4_total');
+const seed_4_name = document.querySelector('#seed_4_name');
+const skip_5_total = document.querySelector('#skip_5_total');
+const clear_5_total = document.querySelector('#clear_5_total');
+const seed_5_name = document.querySelector('#seed_5_name');
+const skip_6_total = document.querySelector('#skip_6_total');
+const clear_6_total = document.querySelector('#clear_6_total');
+const seed_6_name = document.querySelector('#seed_6_name');
+const skip_7_total = document.querySelector('#skip_7_total');
+const clear_7_total = document.querySelector('#clear_7_total');
+const seed_7_name = document.querySelector('#seed_7_name');
+
+skip_1_total_rep.on('change', (newValue, oldValue) => {	
+	skip_1_total.value = newValue;
+});	
+clear_1_total_rep.on('change', (newValue, oldValue) => {	
+	clear_1_total.value = newValue;
+});	
+seed_1_name_rep.on('change', (newValue, oldValue) => {	
+	seed_1_name.value = newValue;
+});	
+skip_2_total_rep.on('change', (newValue, oldValue) => {	
+	skip_2_total.value = newValue;
+});	
+clear_2_total_rep.on('change', (newValue, oldValue) => {	
+	clear_2_total.value = newValue;
+});	
+seed_2_name_rep.on('change', (newValue, oldValue) => {	
+	seed_2_name.value = newValue;
+});	
+skip_3_total_rep.on('change', (newValue, oldValue) => {	
+	skip_3_total.value = newValue;
+});	
+clear_3_total_rep.on('change', (newValue, oldValue) => {	
+	clear_3_total.value = newValue;
+});	
+clear_3_total_rep.on('change', (newValue, oldValue) => {	
+	clear_3_total.value = newValue;
+});	
+seed_3_name_rep.on('change', (newValue, oldValue) => {	
+	seed_3_name.value = newValue;
+});	
+skip_4_total_rep.on('change', (newValue, oldValue) => {	
+	skip_4_total.value = newValue;
+});	
+clear_4_total_rep.on('change', (newValue, oldValue) => {	
+	clear_4_total.value = newValue;
+});	
+seed_4_name_rep.on('change', (newValue, oldValue) => {	
+	seed_4_name.value = newValue;
+});	
+skip_5_total_rep.on('change', (newValue, oldValue) => {	
+	skip_5_total.value = newValue;
+});	
+clear_5_total_rep.on('change', (newValue, oldValue) => {	
+	clear_5_total.value = newValue;
+});	
+seed_5_name_rep.on('change', (newValue, oldValue) => {	
+	seed_5_name.value = newValue;
+});	
+skip_6_total_rep.on('change', (newValue, oldValue) => {	
+	skip_6_total.value = newValue;
+});	
+clear_6_total_rep.on('change', (newValue, oldValue) => {	
+	clear_6_total.value = newValue;
+});	
+seed_6_name_rep.on('change', (newValue, oldValue) => {	
+	seed_6_name.value = newValue;
+});	
+skip_7_total_rep.on('change', (newValue, oldValue) => {	
+	skip_7_total.value = newValue;
+});	
+clear_7_total_rep.on('change', (newValue, oldValue) => {	
+	clear_7_total.value = newValue;
+});	
+seed_7_name_rep.on('change', (newValue, oldValue) => {	
+	seed_7_name.value = newValue;
+});	
+
+updatebutton.onclick = () => {
+    skip_1_total_rep.value = skip_1_total.value;
+    clear_1_total_rep.value = clear_1_total.value;
+    seed_1_name_rep.value = seed_1_name.value;
+    skip_2_total_rep.value = skip_2_total.value;
+    clear_2_total_rep.value = clear_2_total.value;
+    seed_2_name_rep.value = seed_2_name.value;
+    skip_3_total_rep.value = skip_3_total.value;
+    clear_3_total_rep.value = clear_3_total.value;
+    seed_3_name_rep.value = seed_3_name.value;
+    skip_4_total_rep.value = skip_4_total.value;
+    clear_4_total_rep.value = clear_4_total.value;
+    seed_4_name_rep.value = seed_4_name.value;
+    skip_5_total_rep.value = skip_5_total.value;
+    clear_5_total_rep.value = clear_5_total.value;
+    seed_5_name_rep.value = seed_5_name.value;
+    skip_6_total_rep.value = skip_6_total.value;
+    clear_6_total_rep.value = clear_6_total.value;
+    seed_6_name_rep.value = seed_6_name.value;
+    skip_7_total_rep.value = skip_7_total.value;
+    clear_7_total_rep.value = clear_7_total.value;
+    seed_7_name_rep.value = seed_7_name.value;
+}
+
+skip_1_minus.onclick = () => {
+    skip_1_total_rep.value = parseInt(skip_1_total.value) - 1;
+}
+skip_2_minus.onclick = () => {
+    skip_2_total_rep.value = parseInt(skip_2_total.value) - 1;
+}
+skip_3_minus.onclick = () => {
+    skip_3_total_rep.value = parseInt(skip_3_total.value) - 1;
+}
+skip_4_minus.onclick = () => {
+    skip_4_total_rep.value = parseInt(skip_4_total.value) - 1;
+}
+skip_5_minus.onclick = () => {
+    skip_5_total_rep.value = parseInt(skip_5_total.value) - 1;
+}
+skip_6_minus.onclick = () => {
+    skip_6_total_rep.value = parseInt(skip_6_total.value) - 1;
+}
+skip_7_minus.onclick = () => {
+    skip_7_total_rep.value = parseInt(skip_7_total.value) - 1;
+}
+
+skip_1_plus.onclick = () => {
+    skip_1_total_rep.value = parseInt(skip_1_total.value) + 1;
+}
+skip_2_plus.onclick = () => {
+    skip_2_total_rep.value = parseInt(skip_2_total.value) + 1;
+}
+skip_3_plus.onclick = () => {
+    skip_3_total_rep.value = parseInt(skip_3_total.value) + 1;
+}
+skip_4_plus.onclick = () => {
+    skip_4_total_rep.value = parseInt(skip_4_total.value) + 1;
+}
+skip_5_plus.onclick = () => {
+    skip_5_total_rep.value = parseInt(skip_5_total.value) + 1;
+}
+skip_6_plus.onclick = () => {
+    skip_6_total_rep.value = parseInt(skip_6_total.value) + 1;
+}
+skip_7_plus.onclick = () => {
+    skip_7_total_rep.value = parseInt(skip_7_total.value) + 1;
+}
+
+
+clear_1_minus.onclick = () => {
+    clear_1_total_rep.value = parseInt(clear_1_total.value) - 1;
+}
+clear_2_minus.onclick = () => {
+    clear_2_total_rep.value = parseInt(clear_2_total.value) - 1;
+}
+clear_3_minus.onclick = () => {
+    clear_3_total_rep.value = parseInt(clear_3_total.value) - 1;
+}
+clear_4_minus.onclick = () => {
+    clear_4_total_rep.value = parseInt(clear_4_total.value) - 1;
+}
+clear_5_minus.onclick = () => {
+    clear_5_total_rep.value = parseInt(clear_5_total.value) - 1;
+}
+clear_6_minus.onclick = () => {
+    clear_6_total_rep.value = parseInt(clear_6_total.value) - 1;
+}
+clear_7_minus.onclick = () => {
+    clear_7_total_rep.value = parseInt(clear_7_total.value) - 1;
+}
+
+clear_1_plus.onclick = () => {
+    clear_1_total_rep.value = parseInt(clear_1_total.value) + 1;
+}
+clear_2_plus.onclick = () => {
+    clear_2_total_rep.value = parseInt(clear_2_total.value) + 1;
+}
+clear_3_plus.onclick = () => {
+    clear_3_total_rep.value = parseInt(clear_3_total.value) + 1;
+}
+clear_4_plus.onclick = () => {
+    clear_4_total_rep.value = parseInt(clear_4_total.value) + 1;
+}
+clear_5_plus.onclick = () => {
+    clear_5_total_rep.value = parseInt(clear_5_total.value) + 1;
+}
+clear_6_plus.onclick = () => {
+    clear_6_total_rep.value = parseInt(clear_6_total.value) + 1;
+}
+clear_7_plus.onclick = () => {
+    clear_7_total_rep.value = parseInt(clear_7_total.value) + 1;
+}
